042950c3b6b60b8ddb587a751e133ef3486e08ee
sar.puttaraju.ah <sar.puttaraju.ah@SAR-N-PF2795MB>
2022-05-19 20:38:51 +0530

    19-05-2022

--- Chiper_Lab_RFID_System/Chiper_Lab_RFID_System/Variable.cs
+++ Chiper_Lab_RFID_System/Chiper_Lab_RFID_System/Variable.cs
@@ -9,7 +9,7 @@ namespace Chiper_Lab_RFID_System
    public class Variable
     {
         public static RfidManager m_RM = null;
-        public static ProgressDialog ProDialg = null;
+        //public static ProgressDialog ProDialg = null;
         public static string EPC = null;
         public static string Reading = null;
     }
